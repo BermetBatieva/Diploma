15a802e3870f83324cc2da8df435061d1e31f75d
KurenkievTimur <ti-mur99@mail.ru>
2022-06-14 16:20:44 +0600

    Изменил с Crud на JpaRepository

--- src/main/java/com/example/tametable/repository/TimeLessonRepo.java
+++ src/main/java/com/example/tametable/repository/TimeLessonRepo.java
@@ -1,9 +1,9 @@
 package com.example.tametable.repository;
 
 import com.example.tametable.entity.TimeLesson;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TimeLessonRepo extends CrudRepository<TimeLesson, Long> {
+public interface TimeLessonRepo extends JpaRepository<TimeLesson, Long> {
 }
